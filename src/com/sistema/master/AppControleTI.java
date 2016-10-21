c6a573824b8c08acd423557bde43db24e40cef11
comp1 <comp1@comp1_09.lab.unesc.net>
2016-10-21 20:52:38 -0200

    Commit 3

---
+++ src/com/sistema/master/AppControleTI.java
@@ -0,0 +1,22 @@
+
+package com.sistema.master;
+
+import com.sistema.bean.Login;
+import com.sistema.bean.Servico;
+import com.sistema.bean.Cliente;
+
+public class AppControleTI {
+    
+    static Login login[]     = new Login[3];
+    static Cliente cliente[] = new Cliente[20];
+    static Servico servico[] = new Servico[10];
+
+    static int tlogins  = 0; //total login
+    static int tcliente = 0; //total cliente
+    static int tservico = 0; //total servico
+    
+    public static void main(String[] args) {
+        TelaLogin login = new TelaLogin();
+        login.setVisible( true );
+    }
+}
